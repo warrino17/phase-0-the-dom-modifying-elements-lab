36ed38610bf22e0d83d330a7f0f96a865dc58c40
Warren Zhang <warrenzhang17@gmail.com>
2023-03-16 20:12:34 -0400

    DOM lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = "Warren is the champion";
+document.body.append(newHeader);
